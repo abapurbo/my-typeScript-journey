448d22a6c170f802bd28f381388abfd5a5f950c9
apurbo <absarker238@gmail.com>
2026-03-09 12:13:20 +0600

    added typof operator

---
+++ src/typeOf.ts
@@ -0,0 +1,30 @@
+const favoriteMovie = 'Inception';
+
+type movieType = typeof favoriteMovie; //string;
+
+const movie = {
+    title: 'The matrix',
+    director: 'Apurbo sarker',
+};
+
+//Creating a type from a existing object
+type MovieDetails = typeof movie;
+const otherMovie: MovieDetails = {
+    title: 'apurbo',
+    director: 'christorper nolan'
+};
+type detailedLog = {
+    message: string;
+    level: 'info' | 'warn' | 'error'
+};
+
+function logMessage(input: string | number | detailedLog) {
+    if (typeof input === 'string') {
+        console.log('simple log message:', input);
+    }
+    else if (typeof input === "number") {
+        console.log('error code:', input)
+    }else if('message' in input && "level" in input){
+        console.log(`[${input.level.toUpperCase()}]`,input.message)
+    }
+}
